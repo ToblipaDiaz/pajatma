b6909e5d1ff9bb0ffc7e04092b1b932cc918235c
Pablo Diaz <padiaz@intersystems.com>
2024-12-02 15:09:16 -0300

    Subiendo archivos generados para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/pajatma/', // Configura el nombre del repositorio aquí
+export default {
+  base: '/pajatma/',
   plugins: [react()],
   server: {
     port: 5173,
     host: true,
-    open: true
+    open: true,
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'zustand', 'date-fns', 'lucide-react']
-  }
-});
+};
+
